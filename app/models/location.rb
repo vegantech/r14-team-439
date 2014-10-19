072dff2a5e1e8d6953941571cb5ef223a96c0741
Shawn Balestracci <shawn@vegantech.com>
2014-10-19 16:52:27 -0700

    location counts

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,10 @@
 class Location < ActiveRecord::Base
   belongs_to :region
   has_many :github_users
+  has_many :github_push_events, through: :github_users
+
+
+  def self.commit_counts
+    joins(:github_push_events).merge(GithubPushEvent.joins(:github_commits)).group("locations.name").count
+  end
 end
